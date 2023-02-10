323006c59f68613eaa2422798bed20e5dfd42409
Giova <giovanotti1906@hotmail.com>
2023-02-09 20:05:15 -0500

    First commit, just web and security dependences

---
+++ src/test/java/com/mrgiovanotti/securitybasics/SpringSecurityBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrgiovanotti.securitybasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
